823add7e7bf1cd2653dd363f2be6df94bd6b95a5
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-09 16:42:54 -0500

    [nc] use orb managed bash function for awsAssumeRole
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/run_awspec_integration_tests.sh
+++ scripts/run_awspec_integration_tests.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
+source bash-functions.sh
 set -eo pipefail
 
 export ENVIRONMENT=$1
-export AWS_ACCOUNT_ID=$(jq -r .aws_account_id < ${ENVIRONMENT}.auto.tfvars.json)
-export AWS_ASSUME_ROLE=$(jq -r .aws_assume_role < ${ENVIRONMENT}.auto.tfvars.json)
+export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
 
-aws sts assume-role --output json --role-arn arn:aws:iam::"${AWS_ACCOUNT_ID}":role/"${AWS_ASSUME_ROLE}" --role-session-name psk-aws-platform-vpc > credentials
-
-export AWS_ACCESS_KEY_ID=$(jq -r ".Credentials.AccessKeyId" < credentials)
-export AWS_SECRET_ACCESS_KEY=$(jq -r ".Credentials.SecretAccessKey" < credentials)
-export AWS_SESSION_TOKEN=$(jq -r ".Credentials.SessionToken" < credentials)
-export AWS_DEFAULT_REGION=$(jq -r .aws_region < ${ENVIRONMENT}.auto.tfvars.json)
+awsAssumeRole $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id) $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_assume_role)
 
 rspec test/psk_aws_platform_vpc_spec.rb --format documentation
