2dcc4d8eb3e038e59157c4c48afa586e31b9b136
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-05-31 16:52:37 -0500

    [nc] idiomatic jq adjustment
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/run_awspec_integration_tests.sh
+++ scripts/run_awspec_integration_tests.sh
@@ -2,9 +2,11 @@
 source bash-functions.sh
 set -eo pipefail
 
-export ENVIRONMENT=$1
-export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
+export cluster_name=$1
+export aws_account_id=$(jq -er .aws_account_id "$cluster_name".auto.tfvars.json)
+export aws_assume_role=$(jq -er .aws_assume_role "$cluster_name".auto.tfvars.json)
+export AWS_DEFAULT_REGION=$(jq -er .aws_region "$cluster_name".auto.tfvars.json)
 
-awsAssumeRole $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_account_id) $(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_assume_role)
+awsAssumeRole "${aws_account_id}" "${aws_assume_role}"
 
 rspec test/psk_aws_platform_vpc_spec.rb --format documentation
