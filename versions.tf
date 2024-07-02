7d76df9cab23dd19878084077350e19fb6e4d5f1
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-07-01 20:11:17 -0500

    [nc] tf 1.9
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.8"
+  required_version = "~> 1.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.48"
+      version = "~> 5.56"
     }
   }
 
