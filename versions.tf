0ed1cdaeeee8c94a921eb777c889f49b056acc44
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-04-17 17:26:40 -0500

    [nc] tf module upgrade 6.6.1
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- versions.tf
+++ versions.tf
@@ -1,19 +1,25 @@
 terraform {
-  required_version = "~> 1.9"
+  required_version = "~> 1.14"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.94"
+      version = "~> 6.40"
     }
   }
 
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "twplatformlabs"
-    workspaces {
-      prefix = "psk-aws-platform-vpc-"
-    }
-  }
+  # The backend cloud store is managed using the terraform orb tfc-backend command.
+  # this command will generate the appropriate template for the tf workspace:
+  #
+  # terraform {
+  #   cloud {
+  #     organization = "${TFC_ORGANIZATION}"
+  #     hostname = "app.terraform.io"
+
+  #     workspaces {
+  #       name = "${TFC_WORKSPACE}"
+  #     }
+  #   }
+  # }
 }
 
 provider "aws" {
